a2c3867a2f24039e91e03daa78241167adc8efd0
kimmy-hill <95260935+kimmy-hill@users.noreply.github.com>
2022-03-20 16:37:41 -0400

    Delete routes directory

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const apiRoutes = require('./index_api');
-const htmlRoutes = require('./index_html');
-
-router.use('/api', apiRoutes);
-router.use('/', htmlRoutes);
-
-module.exports = router;
